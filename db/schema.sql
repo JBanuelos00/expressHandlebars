d8feeb2dfd91199ba3474a70c6006d5555e81e28
Player 1 <j.banuelos00@gmail.com>
2020-05-25 11:45:33 -0700

    Filled in all code for controllers, orm, models, all handlebars views, css, js files and included the necessary package.json dependencies.

--- db/schema.sql
+++ db/schema.sql
@@ -5,4 +5,8 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
+    id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Morning Star Grillers');
+INSERT INTO burgers (name) VALUES ('Black Bean Chipotle');
+INSERT INTO burgers (name, devoured) VALUES ('Kilauea Veggie Patty', true);
+INSERT INTO burgers (name, devoured) VALUES ('Impossible Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Gardein Burger', true);
+INSERT INTO burgers (name) VALUES ('Portabello Burger');
